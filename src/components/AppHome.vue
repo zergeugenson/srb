28866a89ad3fc50dae39757050d133bc31650203
Sergey <hromov@price.ru>
2021-10-26 13:18:52 +0300

    ++ api works now

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -3,10 +3,12 @@
     <img width="50px" alt="Webpack" src="../images/favicon.png" />
     <input type="text" placeholder="Ваше имя" v-model="name" />
     <input type="text" placeholder="и фамилия" v-model="lastname" />
-    <button @click="send">Проверить JSON</button>
-    <p class="result" v-for="name in employees" :key="name">
+    <button @click="_post">Проверить JSON</button>
+    <p class="result" v-for="name in employees">
       {{ name.name }}, {{ name.lastname }}
     </p>
+    -
+    {{ employees }}
   </div>
 </template>
 
@@ -17,66 +19,33 @@ export default {
     return {
       employees: [],
       name: "",
-      lastname: "",
+      lastname: ""
     };
   },
   mounted() {
-    this.getEmployees();
+    this._get();
   },
   methods: {
-    send() {
-      let xhr = new XMLHttpRequest();
-      let url = "/assets/save.php";
-      xhr.open("POST", url, true);
-      xhr.setRequestHeader("Content-Type", "application/json");
-      let data = JSON.stringify({ name: this.name, lastname: this.lastname });
-      xhr.send(data);
-    },
-    signIn() {
-      let data = JSON.stringify({
-        name: this.name,
-        lastname: this.lastname
-      });
-      let xhr = new XMLHttpRequest();
-      xhr.open('POST', 'http://127.0.0.1:8000/api/v1/auth_token/token/login', false);
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.send(data)
-      if (xhr.status == 200) {
-        localStorage.token = JSON.parse(xhr.responseText).auth_token
-        this.auth = true
-      }
-    },
-    del() {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'http://127.0.0.1:8000/api/v1/articles/list', false);
-      xhr.send()
-    },
-    async getEmployees() {
+    async _get() {
       try {
         const response = await fetch("/assets/data.json");
-        const data = await response.json();
-        this.employees = data;
+        this.employees = await response.json();
       } catch (error) {
         console.error(error);
       }
     },
-    async addEmployee(employee) {
+    async _post() {
       try {
-        const response = await fetch(
-                "/assets/users.json",
-                {
-                  method: "POST",
-                  body: JSON.stringify(employee),
-                  headers: { "Content-type": "application/json; charset=UTF-8" }
-                }
-        );
-        const data = await response.json();
-        this.employees = [...this.employees, data];
-        this.getEmployees();
+        const response = await fetch("/assets/save.php", {
+          method: "POST",
+          body: JSON.stringify({ name: this.name, lastname: this.lastname }),
+          headers: { "Content-type": "application/json; charset=UTF-8" }
+        });
+        this.employees = await response.json();
       } catch (error) {
         console.error(error);
       }
-    },
+    }
   }
 };
 </script>
